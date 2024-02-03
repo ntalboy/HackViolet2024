cdf17f0cd1ccc8f0b9ff27dc7d0224b60564183f
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2024-02-03 11:31:20 -0500

    initial commit

---
+++ src/main/java/com/example/HackViolet/HackVioletApplication.java
@@ -0,0 +1,19 @@
+package com.example.HackViolet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@SpringBootApplication
+public class HackVioletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackVioletApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
+	}
+}
---
+++ src/test/java/com/example/HackViolet/HackVioletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackViolet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackVioletApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
