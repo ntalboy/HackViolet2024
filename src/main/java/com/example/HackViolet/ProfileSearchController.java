4d9624717d39dff555bda49348d661bef676090c
Salvador Buenadicha Calvo <salvadorbu@vt.edu>
2024-02-03 11:57:44 -0500

    frontend backend link

--- src/main/java/com/example/HackViolet/ProfileSearchController.java
+++ src/main/java/com/example/HackViolet/ProfileSearchController.java
@@ -4,11 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
-
-    @GetMapping("/")
+public class ProfileSearchController {
+    @GetMapping("/digging")
     public String index() {
         return "Greetings from Spring Boot!";
     }
-
 }
